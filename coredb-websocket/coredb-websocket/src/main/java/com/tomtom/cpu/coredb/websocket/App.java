36401cfa33ec3ac8e331a0613aa5a45c788b65c1
rk26256 <rahul.gupta@tomtom.com>
2014-01-24 14:50:15 +0530

    Initial Commit

---
+++ coredb-websocket/coredb-websocket/src/main/java/com/tomtom/cpu/coredb/websocket/App.java
@@ -0,0 +1,13 @@
+package com.tomtom.cpu.coredb.websocket;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
